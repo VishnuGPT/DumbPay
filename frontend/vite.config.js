41c58818c9efae20df2f582d52ea7fa011228a9d
VishnuGPT <vishnugpt21@gmail.com>
2025-06-25 21:27:01 +0530

    production changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,23 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//     server: {
+//     host: '192.168.14.19', // binds to local IP
+//     port: 5173,
+//     proxy: {
+//       '/api': 'http://192.168.14.19:3000' // Proxy backend
+//     }
+//   }
+
+// })
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
-    host: '192.168.14.19', // binds to local IP
-    port: 5173,
+  server: {
     proxy: {
-      '/api': 'http://192.168.14.19:3000' // Proxy backend
-    }
-  }
-
+      '/api': 'http://localhost:3000',
+    },
+  },
 })
